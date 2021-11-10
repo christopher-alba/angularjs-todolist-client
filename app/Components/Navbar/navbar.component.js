77f99a5a7f7905d084cb4e510829d5c32972ad4b
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-11 11:11:36 +1300

    style navbar and use controller for links

--- app/Components/Navbar/navbar.component.js
+++ app/Components/Navbar/navbar.component.js
@@ -1,3 +1,11 @@
 angular.module("myApp").component("navbar", {
   templateUrl: "Components/Navbar/navbar.template.html",
+  controller: [
+    "$location",
+    function NavbarController($location) {
+      this.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+      };
+    },
+  ],
 });
