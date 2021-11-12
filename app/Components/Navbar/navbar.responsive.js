e0ef54f581b0273b5a87acd780c24de9d0a875dd
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-13 10:57:42 +1300

    make navbar responsive (incomplete)

---
+++ app/Components/Navbar/navbar.responsive.js
@@ -0,0 +1,29 @@
+const navbarResponsive = () => {
+  console.log("TESTING A");
+  if (window.innerWidth < 750) {
+    console.log("TESTING B");
+    console.log($(".navbar-desktop"));
+    $(".navbar-desktop").hide();
+    $(".navbar-mobile").show();
+    applyMobileStyles();
+  } else {
+    $(".navbar-desktop").show();
+    $(".navbar-mobile").hide();
+    removeMobileStyles();
+  }
+};
+
+const applyMobileStyles = () => {
+  $(".navbar-mobile").addClass("navbar-active");
+};
+const removeMobileStyles = () => {
+  $(".navbar-mobile").removeClass("navbar-active");
+};
+
+window.onload = () => {
+  setTimeout(() => {
+    navbarResponsive();
+  });
+};
+
+window.onresize = navbarResponsive;
