334e74a9bd19969e0f88be9349d58e30f6d98830
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-14 18:26:07 +1300

    write the signup service in app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -26,8 +26,45 @@ app.factory("AuthenticationService", [
 
     service.Login = Login;
     service.Logout = Logout;
+    service.Signup = Signup;
 
     return service;
+
+    function Signup(username, password, callback) {
+      $http({
+        method: "POST",
+        url: Configuration.API + "auth/register",
+        data: {
+          username,
+          password,
+        },
+        headers: { "Content-Type": "application/json" },
+      })
+        .then(function (response) {
+          console.log(response);
+          // login successful if there's a token in the response
+          if (response.data.token) {
+            // store username and token in local storage to keep user logged in between page refreshes
+            $window.localStorage.currentUser = JSON.stringify({
+              username: username,
+              token: response.data.token,
+            });
+
+            // add jwt token to auth header for all requests made by the $http service
+            $http.defaults.headers.common.Authorization =
+              "Bearer " + response.data.token;
+
+            // execute callback with true to indicate successful login
+            callback(true);
+          } else {
+            // execute callback with false to indicate failed login
+            callback(false);
+          }
+        })
+        .catch(() => {
+          console.log("Server is offline, sorry for the inconvenience.");
+        });
+    }
 
     function Login(username, password, callback) {
       $http({
