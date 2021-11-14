732c9df82e4f37024be0b92c218a0881ef88bd36
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-14 22:55:07 +1300

    getting lists api call is working

--- app/Views/todolist/todolist.component.js
+++ app/Views/todolist/todolist.component.js
@@ -1,13 +1,27 @@
 angular.module("todolist").component("todolist", {
   templateUrl: "Views/todolist/todolist.template.html",
   controller: [
-    function Controller() {
+    "ToDoListService",
+    "$window",
+    function Controller(ToDoListService, $window) {
+      const getLists = () => {
+        if ($window.localStorage.currentUser !== undefined) {
+          const username = JSON.parse(
+            $window.localStorage.currentUser
+          ).username;
+          ToDoListService.getLists(username, (lists) => {
+            this.lists = lists;
+          });
+        }
+      };
+
       this.getLists = getLists;
-      this.getListItems = getListItems;
-      this.addList = addList;
-      this.deleteList = deleteList;
-      this.addListItem = addListItem;
-      this.deleteListItem = deleteListItem;
+      // this.getListItems = getListItems;
+      // this.addList = addList;
+      // this.deleteList = deleteList;
+      // this.addListItem = addListItem;
+      // this.deleteListItem = deleteListItem;
+      // this.lists = [];
     },
   ],
 });
--- app/Views/todolist/todolist.service.js
+++ app/Views/todolist/todolist.service.js
@@ -5,21 +5,20 @@ angular.module("todolist").factory("ToDoListService", [
   function Service($http, $window, Configuration) {
     var service = {};
 
-    service.getLists = getLists;
-    service.getListItems = getListItems;
-    service.addList = addList;
-    service.deleteList = deleteList;
-    service.addListItem = addListItem;
-    service.deleteListItem = deleteListItem;
-
     const getLists = (username, callback) => {
       $http({
         method: "GET",
         url: Configuration.API + username + "/lists",
         headers: { "Content-Type": "application/json" },
-      }).then((response) => {
-        callback(response.data.lists);
-      });
+      })
+        .then((response) => {
+          callback(response.data.lists);
+        })
+        .catch(() => {
+          console.log(
+            "The server route doesnt exist yet, sorry for the inconvenience."
+          );
+        });
     };
     const getListItems = (username, listID, callback) => {
       $http({
@@ -34,6 +33,13 @@ angular.module("todolist").factory("ToDoListService", [
     const deleteList = () => {};
     const addListItem = () => {};
     const deleteListItem = () => {};
+
+    service.getLists = getLists;
+    service.getListItems = getListItems;
+    service.addList = addList;
+    service.deleteList = deleteList;
+    service.addListItem = addListItem;
+    service.deleteListItem = deleteListItem;
 
     return service;
   },
